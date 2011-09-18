0b825bdb789760c8b4c89c4bc3a5bcfdb3c5d6a4
Travis La Marr <exiva@exiva.net>
2011-09-17 23:37:40 -0400

    It's alive!

--- venues.php
+++ venues.php
@@ -1,5 +1,10 @@
 <?
-$venue_json = file_get_contents("http://4sq.exiva.net/search-output.json", true);
+$lat = $_GET['lat'];
+$lon = $_GET['lon'];
+$catID = $_GET['cat'];
+$oauth = "HVBP350OQHKOOCN0VD1ZS30VK5EWRDD13XBBKI2GRR4CC2WV";
+
+$venue_json = file_get_contents("https://api.foursquare.com/v2/venues/search?ll=$lat,$lon&categoryId=$catID&oauth_token=$oauth&v=".date('Ydm'), true);
 $venue_json = json_decode($venue_json, true);
 $venue_arr  = $venue_json['response'];
 
@@ -8,12 +13,12 @@ print "<h2>Found ".sizeof($venue_arr['venues'])." matching venues</h2>";
 for($i=0;$i<sizeof($venue_arr['venues']);$i++) {
 	//store in variables
 	$id=$venue_arr['venues'][$i]['id'];
-	if(isset($venue_arr['venues'][$i]['name'])) 					{ $name=$venue_arr['venues'][$i]['name']; } 								else { $missing=True; $name="<strong>Missing Name</strong>";}
-	if(isset($venue_arr['venues'][$i]['location']['crossStreet'])) 	{ $crossSt = "(".$venue_arr['venues'][$i]['location']['crossStreet'].")"; } else { $missing=True; $crossSt = "(<strong>Missing Cross Street</strong>)"; }
-	if(isset($venue_arr['venues'][$i]['location']['address'])) 		{ $address=$venue_arr['venues'][$i]['location']['address']; } 				else { $missing=True; $address="<strong>Missing Address</strong>"; }
-	if(isset($venue_arr['venues'][$i]['location']['city'])) 		{ $city=$venue_arr['venues'][$i]['location']['city'].", "; } 				else { $missing=True; $city="<strong>Missing City</strong>"; }
-	if(isset($venue_arr['venues'][$i]['location']['state'])) 		{ $state=$venue_arr['venues'][$i]['location']['state']; } 					else { $missing=True; $state="<strong>Missing State"; }
-	if(isset($venue_arr['venues'][$i]['location']['postalCode'])) 	{ $zip=$venue_arr['venues'][$i]['location']['postalCode']; } 				else { $missing=True; $zip="<strong>Missing Postal Code</strong>"; }
+	if(isset($venue_arr['venues'][$i]['name'])) { $name=$venue_arr['venues'][$i]['name']; } 								else { $missing=True; $name="<strong>Missing Name</strong>";}
+	if(isset($venue_arr['venues'][$i]['location']['crossStreet'])) { $crossSt = "(".$venue_arr['venues'][$i]['location']['crossStreet'].")"; } else { $missing=True; $crossSt = "(<strong>Missing Cross Street</strong>)"; }
+	if(isset($venue_arr['venues'][$i]['location']['address'])) { $address=$venue_arr['venues'][$i]['location']['address']; } 				else { $missing=True; $address="<strong>Missing Address</strong>"; }
+	if(isset($venue_arr['venues'][$i]['location']['city'])) { $city=$venue_arr['venues'][$i]['location']['city'].", "; } 				else { $missing=True; $city="<strong>Missing City</strong>"; }
+	if(isset($venue_arr['venues'][$i]['location']['state'])) { $state=$venue_arr['venues'][$i]['location']['state']; } 					else { $missing=True; $state="<strong>Missing State"; }
+	if(isset($venue_arr['venues'][$i]['location']['postalCode'])) { $zip=$venue_arr['venues'][$i]['location']['postalCode']; } 				else { $missing=True; $zip="<strong>Missing Postal Code</strong>"; }
 
 	//throw in a notice to edit if some data is missing.
 	if (isset($missing)) {
@@ -40,15 +45,24 @@ for($i=0;$i<sizeof($venue_arr['venues']);$i++) {
 
 	//open links.
 	print "<p>";
-	print "Google It!";
+	if (isset($city) && isset($state)) {
+		print "<a href=\"http://www.google.com/search?q=".urlencode($name." near ".$city.$state)."\">Google It!</a>";
+	} else {
+		print "<a href=\"http://www.google.com/search?q=".urlencode($name)."\">Google It!</a>";
+	}
+	
 	print " | ";
-	print "Yelp It!";
+
+	if (isset($city) && isset($state)) {
+		print "<a href=\"http://www.yelp.com/search?find_desc=".urlencode($name)."&ns=1&find_loc=".urlencode($city.$state)."\">Yelp It!</a>";
+	} else {
+		print "<a href=\"http://www.yelp.com/search?find_desc=".urlencode($name)."&ns=1&find_loc=\">Yelp It!</a>";
+	}
+
 	print " | ";
 	print "<a href=\"https://foursquare.com/v/".$id."\">Open Venue</a>";
 	print "</p>";
 	
 	print "<hr>";
 }
-
-
 ?>
